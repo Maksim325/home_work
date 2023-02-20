2fa244d86774afb8896681593d20f91146ac66d4
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-02-20 19:45:28 +0200

    4 commt

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from untils import data
 
 app = Flask(__name__)
 @app.route("/")
-#основна сторінка 
-def page_index():
+def page_index(): #home page
 	return f'<pre>\n{data[1]["name"]}\n{data[1]["position"]}\n{data[1]["skills"]}\n \n{data[2]["name"]}\n{data[2]["position"]}\n{data[2]["skills"]}\n \n{data[3]["name"]}\n{data[3]["position"]}\n{data[3]["skills"]}\n \n{data[4]["name"]}\n{data[4]["position"]}\n{data[4]["skills"]}\n \n{data[5]["name"]}\n{data[5]["position"]}\n{data[5]["skills"]}\n \n{data[6]["name"]}\n{data[6]["position"]}\n{data[6]["skills"]}\n{data[7]["name"]}\n{data[7]["position"]}\n{data[7]["skills"]}<pre>'
 	
 @app.route("/candidate/<int:id>")
-#пошук по id
-def id_search(id):
+
+def id_search(id): #search by id
 	return f'<img src="{data[id]["picture"]}">\n \n<pre>{data[id]["name"]}\n{data[id]["position"]}\n{data[id]["skills"]}<pre>'
+
+
 @app.route("/skill/<skill>")
 def skill_search(skill):
 	str_candidate = ""
@@ -20,10 +21,10 @@ def skill_search(skill):
 		for x in candidate_skills:
 			x = x.lower()
 
-			print(skill)
-			print(candidate_skills)
 			if skill in x:
 				str_candidate += f'{candidate["name"]} <br>{candidate["position"]}<br> {candidate["skills"]}<br><br>'
 		str_candidate += " "
 	return str_candidate
+
+
 app.run()
